a3208a26ae4f835fb1ca5ac343b0d7942627a3b1
erik schievenin <erik.schievenin@gmail.com>
2022-05-25 22:19:55 +0200

    Create comicController resource CRUD

--- database/seeds/ComicTableSeeder.php
+++ database/seeds/ComicTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Comic;
+
 class ComicTableSeeder extends Seeder
 {
     /**
@@ -11,6 +13,19 @@ class ComicTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data= config('comics');
+        foreach($data as $item){
+            $newComic = new Comic();
+
+            $newComic->thumb = $item['thumb'];
+            $newComic->title = $item['title'];
+            $newComic->series = $item['series'];
+            $newComic->description = $item['description'];
+            $newComic->price = $item['price'];
+            $newComic->sale_date = $item['sale_date'];
+            $newComic->type = $item['type'];
+
+            $newComic->save();
+        }
     }
 }
